4a55fbb34dc785504a6f0e39615c7ecb226b383f
Shawyn <shawynkane@users.noreply.github.com>
2023-04-21 19:41:08 -0500

    Reworked web driver initialization and closing. Added forgot password test case for login test suite.

--- src/main/java/LoginTestSuite.java
+++ src/main/java/LoginTestSuite.java
@@ -1,14 +1,18 @@
 import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LoginTestSuite extends TestSuite {
 
 
-    private final static String incorrectPasswordMessage = "* Incorrect username or password";
+    private final static String INCORRECT_PASSWORD_MESSAGE = "* Incorrect username or password";
+    private final static String FORGOT_PASSWORD_MESSAGE = "Please use temporary password 'rahulshettyacademy' to Login.";
 
     @Test
     public void passwordErrorMessageInChromeTestCase() {
+
+        ChromeDriver chromeDriver = initializeChromeDriver();
 
         try {
 
@@ -25,7 +29,43 @@ public class LoginTestSuite extends TestSuite {
 
         } // try catch
 
-        Assert.isTrue(chromeDriver.findElement(By.cssSelector("p.error")).getText().equals(incorrectPasswordMessage), "Error message is not correct.");
-
+        try {
+            Assert.isTrue(chromeDriver.findElement(By.cssSelector("p.error")).getText().equals(INCORRECT_PASSWORD_MESSAGE), "Error message is not correct.");
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            chromeDriver.close();
+        }
     } // passwordErrorMessageTestCase
+
+    @Test
+    public void forgotPasswordTestCase() {
+        ChromeDriver chromeDriver = initializeChromeDriver();
+
+        try {
+
+            setWebDriverURL("https://rahulshettyacademy.com/locatorspractice/", chromeDriver);
+            chromeDriver.findElement(By.linkText("Forgot your password?")).click();
+            chromeDriver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("NewTestUser");
+            chromeDriver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("newTestUser12345@test.test");
+            chromeDriver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("234-567-8901");
+            chromeDriver.findElement(By.className("reset-pwd-btn")).click();
+            
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        } // try catch
+
+        try {
+
+            Assert.isTrue(chromeDriver.findElement(By.className("infoMsg")).getText().equals(FORGOT_PASSWORD_MESSAGE), "Forgot Password message is not correct.");
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            chromeDriver.close();
+        }
+    }
+
 }
--- src/main/java/TestSuite.java
+++ src/main/java/TestSuite.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,26 +12,20 @@ import java.util.Properties;
 
 public abstract class TestSuite {
     protected final static String rahulAcademyURL = "https://rahulshettyacademy.com/";
-    protected ChromeDriver chromeDriver = null;
-    protected Properties chromeWebdriverProperties = new Properties();
 
-    TestSuite() { initialization(); }
-
-    private void loadInitialGeneralProperties() {
+    private Properties loadChromeWebDriverProperties() {
         try {
+            Properties chromeWebdriverProperties = new Properties();
             FileReader testSuitePropertiesFile = new FileReader("src/main/resources/chromeWebdriver");
             chromeWebdriverProperties.load(testSuitePropertiesFile);
             testSuitePropertiesFile.close();
+            return chromeWebdriverProperties;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private void initialization() {
-        loadInitialGeneralProperties();
-        chromeDriver = initializeChromeDriver();
+        return null;
     }
 
     protected boolean checkCurrentURL(String expectedURL, WebDriver driver) {
@@ -42,18 +38,19 @@ public abstract class TestSuite {
     }
 
     protected ChromeDriver initializeChromeDriver() {
+        return initializeChromeDriver(Duration.ofSeconds(5));
+    }
+
+    protected ChromeDriver initializeChromeDriver(Duration implicitWait) {
+        Properties chromeWebdriverProperties = loadChromeWebDriverProperties();
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=" + chromeWebdriverProperties.getProperty("--remote-allow-origins"));
 
         System.out.println(chromeWebdriverProperties.getProperty("webdriver.chrome.driver"));
         System.setProperty("webdriver.chrome.driver", chromeWebdriverProperties.getProperty("webdriver.chrome.driver"));
         ChromeDriver newChromeDriver = new ChromeDriver(options);
-        newChromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        newChromeDriver.manage().timeouts().implicitlyWait(implicitWait);
         return newChromeDriver;
-    }
-
-    public boolean close() {
-        chromeDriver.quit();
-        return true;
     }
 }
